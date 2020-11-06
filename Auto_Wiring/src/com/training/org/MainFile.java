0554469de05af3fb80fb533ee51d3ec5536e5fa4
Haswika-0711 <bommena.haswika7@gmail.com>
2020-11-06 16:57:26 +0530

    "Added files"

---
+++ Auto_Wiring/src/com/training/org/MainFile.java
@@ -0,0 +1,22 @@
+package com.training.org;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class MainFile {
+	public static void main(String[] args) {
+		
+	
+		ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		A a=context.getBean("aBean",A.class);
+		a.display();
+		
+		
+	}
+
+}
